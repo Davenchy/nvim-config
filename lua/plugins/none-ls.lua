46171bd35a791dbb372bc91e4be39f787c394a88
Fadi Asaad <firon1222@gmail.com>
2024-05-09 10:53:07 +0300

    add none-ls alternative for null-ls for code linting and formatting

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,33 @@
+-- replacment for null-ls
+return {
+  "nvimtools/none-ls.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local null_ls = require("null-ls")
+
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.completion.spell,
+        null_ls.builtins.completion.luasnip,
+
+        null_ls.builtins.diagnostics.checkmake,
+        null_ls.builtins.diagnostics.commitlint,
+        null_ls.builtins.diagnostics.cppcheck,
+        null_ls.builtins.diagnostics.pylint,
+        null_ls.builtins.diagnostics.trivy,
+        null_ls.builtins.diagnostics.eslint_d,
+
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.isortd,
+        null_ls.builtins.formatting.blackd,
+        null_ls.builtins.formatting.clang_format,
+        null_ls.builtins.formatting.markdownlint,
+        null_ls.builtins.formatting.prettierd,
+
+        null_ls.builtins.hover.printenv,
+      },
+    })
+
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "LSP Format" })
+  end,
+}
