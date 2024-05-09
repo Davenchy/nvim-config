0b88b7b3d5428a01cfe94db40c43fb45a30c2ef8
Fadi Asaad <firon1222@gmail.com>
2024-05-09 13:36:31 +0300

    update which-key with new keymaps for buffer, diagonistics and code utils

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -12,7 +12,36 @@ return {
     wk.register({
       ['<leader>'] = {
         f = { name = "File" },
-        c = { name = "Code" },
+        c = {
+          name = "Code",
+          i = { vim.lsp.buf.implementation, "Implementation" },
+          r = { vim.lsp.buf.refrences, "Refrences" },
+          n = { vim.lsp.buf.rename, "Rename" },
+          t = { vim.lsp.buf.type_definition, "Type Definition" },
+          h = { vim.lsp.buf.signature_help, "Signature Help" },
+          m = { vim.lsp.buf.hover, "Show Docs/Manual" },
+          s = { vim.lsp.buf.document_symbol, "Symbols" },
+          f = { vim.lsp.buf.format, "Format" },
+          d = { vim.lsp.buf.definition, "Go To Definition" },
+          c = { vim.lsp.buf.declaration, "Go To Declaration" },
+          a = { vim.lsp.buf.code_action, "Code Actions" },
+        },
+        b = {
+          name = "Buffer",
+          s = { ':b#<CR>', "Switch" },
+          n = { ':new<CR>', "New" },
+          b = { ':Telescope buffers<CR>', "Buffers" },
+          c = { ':bdelete<CR>', "Close" },
+        },
+        x = {
+          name = "Diagnostics",
+          x = { ':TroubleToggle<CR>', "Toggle Trouble" },
+          w = { ':TroubleToggle workspace_diagnostics<CR>', "Workspace Diagnostics" },
+          d = { ':TroubleToggle document_diagnostics<CR>', "Document Diagnostics" },
+          q = { ':TroubleToggle quickfix<CR>', "Trouble QuickFix" },
+          l = { ':TroubleToggle loclist<CR>', "Trouble LocList" },
+          t = { ':TodoTrouble<CR>', "Trouble Todo Comments" },
+        },
       },
     })
   end,
