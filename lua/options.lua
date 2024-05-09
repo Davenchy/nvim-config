33ca7bbd4df89208292a13788c1db6916b5dfc1b
Fadi Asaad <firon1222@gmail.com>
2024-05-09 05:21:42 +0300

    add vim options

---
+++ lua/options.lua
@@ -0,0 +1,9 @@
+vim.opt.background = 'dark'
+vim.opt.number = true
+vim.opt.expandtab = true -- use spaces
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.wrap = false
+vim.opt.colorcolumn = '+1'
+vim.opt.hidden = true
+vim.g.leader = "space"
