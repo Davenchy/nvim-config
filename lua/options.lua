5faf8ac2331647f1663f8f186a27a4492a887102
Fadi Asaad <firon1222@gmail.com>
2024-05-12 13:29:19 +0300

    update editor global options

--- lua/options.lua
+++ lua/options.lua
@@ -1,12 +1,33 @@
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.g.have_nerd_font = true
 
 vim.opt.background = 'dark'
 vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = 'yes'
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.splitright = true
+vim.opt.splitbelow = true
 vim.opt.expandtab = true -- use spaces
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.wrap = false
 vim.opt.colorcolumn = '+1'
+vim.opt.scrolloff = 5
 vim.opt.hidden = true
 vim.opt.wildmode = { 'longest', 'full' }
 vim.opt.wildoptions = 'fuzzy'
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+vim.opt.hlsearch = true
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
