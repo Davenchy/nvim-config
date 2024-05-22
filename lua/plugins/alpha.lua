3198275f8a06eba5d7cda59bbfe9355b5e9eda52
Fadi Asaad <firon1222@gmail.com>
2024-05-22 11:43:48 +0300

    update alpha buttons

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,60 @@
+return {
+  'goolord/alpha-nvim',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+    'nvim-lua/plenary.nvim'
+  },
+  config = function()
+    local theta = require 'alpha.themes.theta'
+    local button = require 'alpha.themes.dashboard'.button
+    local buttons = {
+      type = "group",
+      val = {
+        { type = "text",    val = "Quick links", opts = { hl = "SpecialComment", position = "center" } },
+        { type = "padding", val = 1 },
+        button("e", "  New file", "<cmd>ene<CR>"),
+        button("SPC s f", "󰈞  Find file"),
+        button("SPC s g", "󰊄  Live grep"),
+        button("c", "  Configuration", "<cmd>cd ~/.config/nvim/ <CR>"),
+        button("u", "  Update plugins", "<cmd>Lazy sync<CR>"),
+        button("q", "󰅚  Quit", "<cmd>qa<CR>"),
+      },
+      position = "center",
+    }
+    local mru = {
+      type = "group",
+      val = {
+        {
+          type = "text",
+          val = "Recent files",
+          opts = {
+            hl = "SpecialComment",
+            shrink_margin = false,
+            position = "center",
+          },
+        },
+        { type = "padding", val = 1 },
+        {
+          type = "group",
+          val = function()
+            return { theta.mru(0, vim.fn.getcwd()) }
+          end,
+          opts = { shrink_margin = false },
+        },
+      },
+    }
+
+
+    require 'alpha'.setup({
+      layout = {
+        { type = "padding", val = 2 },
+        theta.config.header,
+        { type = "padding", val = 2 },
+        mru,
+        { type = "padding", val = 2 },
+        buttons,
+      },
+      opts = theta.config.opts,
+    })
+  end
+}
--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -9,16 +9,6 @@ return {
     opts = {},
   },
   {
-    'goolord/alpha-nvim',
-    dependencies = {
-      'nvim-tree/nvim-web-devicons',
-      'nvim-lua/plenary.nvim'
-    },
-    config = function()
-      require 'alpha'.setup(require 'alpha.themes.theta'.config)
-    end
-  },
-  {
     'windwp/nvim-autopairs',
     event = 'InsertEnter',
     -- Optional dependency
