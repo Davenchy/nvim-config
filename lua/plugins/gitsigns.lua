fda5a02055154668b6a4f41f96262a68b7ee0419
Fadi Asaad <firon1222@gmail.com>
2024-05-28 10:05:11 +0300

    update gitsigns options

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,6 +2,8 @@ return {
   {
     'lewis6991/gitsigns.nvim',
     opts = {
+      numhl = true,
+      current_line_blame = true,
       signs = {
         add = { text = '+' },
         change = { text = '~' },
