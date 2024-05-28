155381b20180baadf5d5ba92bb626ec380941b2e
Fadi Asaad <firon1222@gmail.com>
2024-05-28 10:17:17 +0300

    add Oil and update all plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -103,4 +103,12 @@ return {
     },
     opts = {},
   },
+  {
+    'stevearc/oil.nvim',
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      {'-', '<CMD>Oil<CR>', desc = 'Open parent directory'},
+    },
+    opts = {},
+  }
 }
