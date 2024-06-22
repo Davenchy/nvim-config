1354a877a75f12e088ade52699e9fc53051271c4
Fadi Asaad <firon1222@gmail.com>
2024-06-23 00:11:30 +0300

    add formatting config

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,29 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "isort", "black" },
+      javascript = { { "prettierd", "prettier" }, "biome" },
+      typescript = { "biome" },
+    },
+    format_on_save = {
+      lsp_fallback = true,
+      async = false,
+      timeout_ms = 5000,
+    },
+  },
+  config = function(_, opts)
+    local conform = require("conform")
+    conform.setup(opts)
+
+    vim.keymap.set({ "n", "v" }, "<leader>cf", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 5000,
+      })
+    end, { desc = "Code Format" })
+  end,
+}
