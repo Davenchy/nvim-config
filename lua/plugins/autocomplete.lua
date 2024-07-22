e19d3bc62314b0d106db335e7dbe5464df4fc054
Fadi Asaad <firon1222@gmail.com>
2024-07-22 13:08:26 +0300

    format: autocomplete.lua

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -11,12 +11,12 @@ return {
     "https://codeberg.org/FelipeLema/cmp-async-path",
   },
   config = function()
-    local cmp = require "cmp"
+    local cmp = require("cmp")
 
-    cmp.setup {
+    cmp.setup({
       snippet = {
         expand = function(args)
-          require 'luasnip'.lsp_expand(args.body)
+          require("luasnip").lsp_expand(args.body)
         end,
       },
       window = {
@@ -24,69 +24,76 @@ return {
         documentation = cmp.config.window.bordered(),
       },
       mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        ['<C-l>'] = cmp.mapping(function()
-          local ls = require 'luasnip'
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-l>"] = cmp.mapping(function()
+          local ls = require("luasnip")
           if ls.expand_or_locally_jumpable() then
             ls.expand_or_jump()
           end
-        end, { 'i', 's' }),
-        ['<C-h>'] = cmp.mapping(function()
-          local ls = require 'luasnip'
+        end, { "i", "s" }),
+        ["<C-h>"] = cmp.mapping(function()
+          local ls = require("luasnip")
           if ls.locally_jumpable(-1) then
             ls.jump(-1)
           end
-        end, { 'i', 's' }),
+        end, { "i", "s" }),
+        ["<C-k>"] = function()
+          if cmp.visible_docs() then
+            cmp.close_docs()
+          else
+            cmp.open_docs()
+          end
+        end,
       }),
       sources = cmp.config.sources({
-        { name = 'calc' },
-        { name = 'codeium' },
-        { name = 'nvim_lsp' },
-        { name = 'treesitter' },
+        { name = "calc" },
+        { name = "codeium" },
+        { name = "nvim_lsp" },
+        { name = "treesitter" },
         {
-          name = 'luasnip',
+          name = "luasnip",
           option = {
             show_autosnippets = true,
             use_show_condition = false,
           },
         },
-        { name = 'emoji' },
+        { name = "emoji" },
       }, {
-        { name = 'async_path' },
-        { name = 'buffer' },
+        { name = "async_path" },
+        { name = "buffer" },
       }),
       formatting = {
-        format = require 'lspkind'.cmp_format {
-          mode = 'symbol_text',
+        format = require("lspkind").cmp_format({
+          mode = "symbol_text",
           symbol_map = {
             Codeium = "",
           },
-        },
+        }),
       },
       experimental = { ghost_text = true },
-    }
+    })
 
     -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-    cmp.setup.cmdline({ '/', '?' }, {
+    cmp.setup.cmdline({ "/", "?" }, {
       mapping = cmp.mapping.preset.cmdline(),
       sources = {
-        { name = 'buffer' },
-      }
+        { name = "buffer" },
+      },
     })
 
     -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-    cmp.setup.cmdline(':', {
+    cmp.setup.cmdline(":", {
       mapping = cmp.mapping.preset.cmdline(),
       sources = cmp.config.sources({
-        { name = 'cmdline' },
-        { name = 'async_path' },
-        { name = 'buffer' },
+        { name = "cmdline" },
+        { name = "async_path" },
+        { name = "buffer" },
       }),
-      matching = { disallow_symbol_nonprefix_matching = false }
+      matching = { disallow_symbol_nonprefix_matching = false },
     })
   end,
 }
