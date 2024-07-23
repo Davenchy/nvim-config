878b08eba101c8f990ca063a39c0fe184e5bb7b1
Fadi Asaad <firon1222@gmail.com>
2024-07-24 02:59:55 +0300

    update: which-key config to v3

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,53 +1,80 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-  end,
-  config = function()
-    local wk = require "which-key"
-    wk.setup {}
-
-    wk.register({
-      ['<leader>'] = {
-        s = { name = "[S]earch" },
-        u = { name = "[U]I" },
-        h = { name = '[H]unks' },
-        c = {
-          name = "[C]ode",
-          i = { vim.lsp.buf.implementation, "Implementation" },
-          r = { vim.lsp.buf.refrences, "Refrences" },
-          n = { vim.lsp.buf.rename, "Rename" },
-          t = { vim.lsp.buf.type_definition, "Type Definition" },
-          k = { vim.lsp.buf.signature_help, "Signature Help" },
-          h = { vim.lsp.buf.hover, "Show Docs/Manual for [h]overed" },
-          s = { vim.lsp.buf.document_symbol, "Symbols" },
-          f = { vim.lsp.buf.format, "Format" },
-          d = { vim.lsp.buf.definition, "Go To Definition" },
-          c = { vim.lsp.buf.declaration, "Go To Declaration" },
-          a = { vim.lsp.buf.code_action, "Code Actions" },
-        },
-        b = {
-          name = "[B]uffer",
-          b = { ':b#<CR>', "Buffer: Switch With Last previous" },
-          n = { ':new<CR>', "Buffer: Create [N]ew" },
-          v = { ':vsplit new<CR>', "Buffer: Create New [V]ertical" },
-          h = { ':split new<CR>', "Buffer: Create New [H]orizontal" },
-          c = { ':bdelete<CR>', "Buffer: [C]lose" },
-        },
-        d = {
-          name = "[D]ocument",
-          d = { ':TroubleToggle document_diagnostics<CR>', "Document Diagnostics" },
-          q = { ':TroubleToggle quickfix<CR>', "Trouble QuickFix" },
-          t = { ':TodoTrouble<CR>', "Trouble Todo Comments" },
-        },
-        t = {
-          name = "[T]erminal",
-          t = { ':FTermToggle<CR>', "Toggle floating terminal" },
-          l = { function() require 'FTerm'.run{'lazygit'} end, "LazyGit" },
-        }
+  opts = {
+    spec = {
+      { "<leader>h", group = "Git" },
+      { "<leader>s", group = "Search" },
+      { "<leader>t", group = "Terminal" },
+      { "<leader>u", group = "UI" },
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps",
       },
-    })
-  end,
+      {
+        "<leader>b",
+        group = "Buffer",
+        expand = function()
+          return require("which-key.extras").expand.buf()
+        end,
+      },
+      { "<leader>bb", ":b#<CR>", desc = "Buffer: Switch With Last previous" },
+      { "<leader>bc", ":bdelete<CR>", desc = "Buffer: [C]lose" },
+      {
+        "<leader>bh",
+        ":split new<CR>",
+        desc = "Buffer: Create New [H]orizontal",
+      },
+      { "<leader>bn", ":new<CR>", desc = "Buffer: Create [N]ew" },
+      {
+        "<leader>bv",
+        ":vsplit new<CR>",
+        desc = "Buffer: Create New [V]ertical",
+      },
+      { "<leader>c", group = "Code" },
+      { "<leader>ci", vim.lsp.buf.implementation, desc = "Implementation" },
+      { "<leader>cr", vim.lsp.buf.refrences, desc = "Refrences" },
+      { "<leader>cn", vim.lsp.buf.rename, desc = "Rename" },
+      { "<leader>ct", vim.lsp.buf.type_definition, desc = "Type Definition" },
+      { "<leader>ck", vim.lsp.buf.signature_help, desc = "Signature Help" },
+      {
+        "<leader>ch",
+        vim.lsp.buf.hover,
+        desc = "Show Docs/Manual for [h]overed",
+      },
+      { "<leader>cs", vim.lsp.buf.document_symbol, desc = "Symbols" },
+      { "<leader>cf", vim.lsp.buf.format, desc = "Format" },
+      { "<leader>cd", vim.lsp.buf.definition, desc = "Go To Definition" },
+      { "<leader>cc", vim.lsp.buf.declaration, desc = "Go To Declaration" },
+      { "<leader>ca", vim.lsp.buf.code_action, desc = "Code Actions" },
+      { "<leader>x", group = "Diagnostics" },
+      {
+        "<leader>xx",
+        ":Trouble diagnostics toggle filter.buf=0<CR>",
+        desc = "Document Diagnostics",
+      },
+      {
+        "<leader>xq",
+        ":Trouble quickfix toggle<CR>",
+        desc = "Trouble QuickFix",
+      },
+      {
+        "<leader>xt",
+        ":TodoTrouble toggle<CR>",
+        desc = "Trouble Todo Comments",
+      },
+      { "<leader>xs", ":Trouble symbols toggle<CR>", desc = "Symbols" },
+      {
+        "<leader>tl",
+        function()
+          require("FTerm").scratch({ cmd = { "lazygit" } })
+        end,
+        desc = "LazyGit",
+      },
+      { "<leader>tt", ":FTermToggle<CR>", desc = "Toggle floating terminal" },
+    },
+  },
 }
