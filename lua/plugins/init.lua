b5ad913658814267bdc149295a4b9ec6f385ac75
Fadi Asaad <firon1222@gmail.com>
2024-07-24 03:08:58 +0300

    update: oil.vim package lazy load to load on command execute

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -106,6 +106,7 @@ return {
   {
     "stevearc/oil.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    cmd = "Oil",
     keys = {
       { "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
     },
