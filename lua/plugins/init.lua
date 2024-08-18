59a876f94ff1aa209bd2a3486c3e6309d35fe7bb
Fadi Asaad <firon1222@gmail.com>
2024-08-18 05:25:05 +0300

    Add `noice` for better UI

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -113,5 +113,24 @@ return {
     opts = {},
   },
   "voldikss/vim-floaterm",
+  {
+    "stevearc/dressing.nvim",
+    opts = {},
+  },
   "rcarriga/nvim-notify",
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    },
+  },
 }
