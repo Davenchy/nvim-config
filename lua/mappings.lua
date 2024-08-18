91546d4d2f8d067cce2bbca46b94315fecb7b010
Fadi Asaad <firon1222@gmail.com>
2024-08-18 05:28:13 +0300

    Reformat mappings.lua and options.lua

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,52 +4,52 @@
 
 -- create mapping helper functions
 local function map(keys, func, desc)
-  vim.keymap.set('n', keys, func, { desc = desc })
+  vim.keymap.set("n", keys, func, { desc = desc })
 end
 local function mmap(mode, keys, func, desc)
   vim.keymap.set(mode, keys, func, { desc = desc })
 end
 
 -- Save files using Ctrl + s, in normal/insert modes
-mmap({ 'n', 'i' }, '<C-s>', '<Esc>:w<CR>', 'Save File')
+mmap({ "n", "i" }, "<C-s>", "<Esc>:w<CR>", "Save File")
 
 -- Source file using Ctrl + S
-map('<C-S-s>', ':so %<CR>', 'Source File')
+map("<C-S-s>", ":so %<CR>", "Source File")
 
 -- Close buffer using Ctrl + c
-map('<C-x>', ':bdelete<CR>', "Close Buffer")
+map("<C-x>", ":bdelete<CR>", "Close Buffer")
 
 -- Quit Neovim
-map('<C-q>', ':qa<CR>', "Quit Neovim")
+map("<C-q>", ":qa<CR>", "Quit Neovim")
 
-map('<Esc>', ':nohl<CR>', "Clear all search highlights")
-mmap('t', '<Esc><Esc>', '<C-\\><C-n>', 'Exit terminal mode')
+map("<Esc>", ":nohl<CR>", "Clear all search highlights")
+mmap("t", "<Esc><Esc>", "<C-\\><C-n>", "Exit terminal mode")
 
 -- Diagnostic keymaps
-map('[d', vim.diagnostic.goto_prev, 'Go to previous [D]iagnostic message')
-map(']d', vim.diagnostic.goto_next, 'Go to next [D]iagnostic message')
+map("[d", vim.diagnostic.goto_prev, "Go to previous [D]iagnostic message")
+map("]d", vim.diagnostic.goto_next, "Go to next [D]iagnostic message")
 
 -- Switch between windows using arrow keys
-map('<C-right>', '<C-w><C-l>', 'Focus right window')
-map('<C-left>', '<C-w><C-h>', 'Focus left window')
-map('<C-up>', '<C-w><C-k>', 'Focus up window')
-map('<C-down>', '<C-w><C-j>', 'Focus down window')
+map("<C-right>", "<C-w><C-l>", "Focus right window")
+map("<C-left>", "<C-w><C-h>", "Focus left window")
+map("<C-up>", "<C-w><C-k>", "Focus up window")
+map("<C-down>", "<C-w><C-j>", "Focus down window")
 
 -- Resize windows using arrow keys
-map('<C-S-right>', '<C-w>>', 'Focus right window')
-map('<C-S-left>', '<C-w><', 'Focus left window')
-map('<C-S-up>', '<C-w>+', 'Focus up window')
-map('<C-S-down>', '<C-w>-', 'Focus down window')
+map("<C-S-right>", "<C-w>>", "Focus right window")
+map("<C-S-left>", "<C-w><", "Focus left window")
+map("<C-S-up>", "<C-w>+", "Focus up window")
+map("<C-S-down>", "<C-w>-", "Focus down window")
 
 -- Switch tabs using <C-tab>
-map('<C-tab>', 'gt', 'Goto next tab')
-map('<C-S-tab>', 'gT', 'Goto previous tab')
-map('<C-n>', ':tabnew<CR>', 'Create a new tab')
-map('<C-c>', ':tabclose<CR>', 'Close tab')
+map("<C-tab>", "gt", "Goto next tab")
+map("<C-S-tab>", "gT", "Goto previous tab")
+map("<C-n>", ":tabnew<CR>", "Create a new tab")
+map("<C-c>", ":tabclose<CR>", "Close tab")
 
 -- Open Terminals
-map('<leader>|', ':vsplit +term<CR>:start<CR>', "Open Vertical Terminal")
-map('<leader>_', ':split +term<CR>:start<CR>', "Open Horizontal Terminal")
+map("<leader>|", ":vsplit +term<CR>:start<CR>", "Open Vertical Terminal")
+map("<leader>_", ":split +term<CR>:start<CR>", "Open Horizontal Terminal")
 
 -- Toggle ZenMode
-map('<leader>z', ':ZenMode<CR>', "Toggle ZenMode")
+map("<leader>z", ":ZenMode<CR>", "Toggle ZenMode")
--- lua/options.lua
+++ lua/options.lua
@@ -1,19 +1,19 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 vim.g.have_nerd_font = true
 
-vim.opt.background = 'dark'
+vim.opt.background = "dark"
 vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.confirm = true
-vim.opt.mouse = 'a'
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
 vim.opt.breakindent = true
 vim.opt.undofile = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 vim.opt.showmode = true
 vim.opt.showcmd = true
 vim.opt.ruler = true
@@ -26,14 +26,14 @@ vim.opt.expandtab = true -- use spaces
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.wrap = false
-vim.opt.colorcolumn = '+1'
+vim.opt.colorcolumn = "+1"
 vim.opt.scrolloff = 5
 vim.opt.hidden = true
-vim.opt.wildmode = { 'longest', 'full' }
-vim.opt.wildoptions = 'fuzzy'
+vim.opt.wildmode = { "longest", "full" }
+vim.opt.wildoptions = "fuzzy"
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 vim.opt.hlsearch = true
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
