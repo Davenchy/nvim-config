d378b645255236a155a541133c76bf944ecc52a2
Fadi Asaad <firon1222@gmail.com>
2024-12-09 08:23:29 +0200

    reformat the commands.lua file

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,16 +1,19 @@
 -- Highlight when yanking (copying) text
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup(
+    "kickstart-highlight-yank",
+    { clear = true }
+  ),
   callback = function()
     vim.highlight.on_yank()
   end,
 })
 
 -- Disable numbers in terminal mode
-vim.api.nvim_create_autocmd('TermOpen', {
-  desc = 'Disable numbers in terminal mode',
-  group = vim.api.nvim_create_augroup('kickstart-terminal', { clear = true }),
+vim.api.nvim_create_autocmd("TermOpen", {
+  desc = "Disable numbers in terminal mode",
+  group = vim.api.nvim_create_augroup("kickstart-terminal", { clear = true }),
   callback = function()
     vim.opt_local.number = false
     vim.opt_local.relativenumber = false
@@ -18,9 +21,9 @@ vim.api.nvim_create_autocmd('TermOpen', {
 })
 
 -- Define user command to copy the code aas a screenshot using silicon cli tool
-vim.api.nvim_create_user_command('CopyScreen', function()
-  local filepath = vim.fn.expand('%')
-  os.execute('silicon -c ' .. filepath)
+vim.api.nvim_create_user_command("CopyScreen", function()
+  local filepath = vim.fn.expand("%")
+  os.execute("silicon -c " .. filepath)
 end, {
   desc = "Copy the code as a screenshot using silicon cli tool",
 })
