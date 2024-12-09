9d675ba17edd40982947efc301396cfe9600ccac
Fadi Asaad <firon1222@gmail.com>
2024-12-09 08:11:44 +0200

    fix biome formatting

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -5,14 +5,19 @@ return {
     formatters_by_ft = {
       lua = { "stylua" },
       python = { "autoflake", "docformatter", "isort" },
-      javascript = { "biome" },
-      typescript = { "biome" },
+      javascript = { "biome", "prettierd" },
+      typescript = { "biome", "prettierd" },
       markdown = { "mdformat", "prettierd", "markdownlint" },
     },
     format_on_save = {
       lsp_fallback = true,
       async = false,
       timeout_ms = 5000,
+    },
+    formatters = {
+      biome = {
+        require_cwd = true,
+      },
     },
   },
   config = function(_, opts)
