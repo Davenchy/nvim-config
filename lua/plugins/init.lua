681391d08ee1641fb0fe51c74bfee98a2998a9c7
Fadi Asaad <firon1222@gmail.com>
2025-01-09 14:05:13 +0200

    remove nvim-cmp dependency from nvim-autopairs

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -11,15 +11,9 @@ return {
   {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
-    -- Optional dependency
-    dependencies = { "hrsh7th/nvim-cmp" },
-    config = function()
-      require("nvim-autopairs").setup({})
-      -- If you want to automatically add `(` after selecting a function or method
-      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-      local cmp = require("cmp")
-      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-    end,
+    opts = {
+      map_cr = true,
+    },
   },
   {
     "nvim-lualine/lualine.nvim",
