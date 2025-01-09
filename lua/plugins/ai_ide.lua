3c9f5269aaaa71a849e3e1c0cd6b3216fa4ad64e
Fadi Asaad <firon1222@gmail.com>
2025-01-09 14:06:18 +0200

    add avante.nvim for enhanced ai support like cursor-ai editor

--- lua/plugins/ai_ide.lua
+++ lua/plugins/ai_ide.lua
@@ -5,4 +5,39 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = { enable_chat = true },
   },
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "zbirenbaum/copilot.lua",
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+    opts = {
+      provider = "copilot",
+      auto_suggestions_provider = "copilot",
+      behavior = {
+        auto_suggestions = true,
+      },
+      windows = {
+        sidebar_header = {
+          enabled = false,
+        },
+        ask = {
+          floating = true,
+        },
+      },
+    },
+  },
 }
